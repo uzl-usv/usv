1cf11420e1c466d08f74ce35526ea019ae8bb1f8
josib <josephinebrauer@gmx.de>
2021-03-17 12:15:48 +0100

    Script for setting environment vars

---
+++ scripts/export_vars.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+export file_name=test1
+export origin_lat=-30.048638	#default: -30.048638
+export origin_lon=-51.236690	#default: -51.236690
+export start_lat=-30.048638	#default: -30.048638
+export start_lon=-51.236690	#default: -51.236690
+export home_lat=-30.047358	#default: -30.047358
+export home_lon=-51.233064	#default: -51.233064
+export width=15			#default: 15
+export height=15		#default: 15
+export dist=5			#default: 5
